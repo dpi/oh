275901a1d089348ab7dbe5ed5fceaa935c0a5a1b
dpi <pro@danielph.in>
2018-06-18 01:29:21 +0800

    Add binary open/closed to occurrence
    Closes dpi/oh#5

--- src/OhOccurrence.php
+++ src/OhOccurrence.php
@@ -20,6 +20,13 @@ class OhOccurrence extends OhDateRange implements RefinableCacheableDependencyIn
   protected $message;
 
   /**
+   * Whether this occurrence is open.
+   *
+   * @var bool
+   */
+  protected $open = FALSE;
+
+  /**
    * Set the message for the occurrence.
    *
    * @param string|null $message
@@ -41,6 +48,30 @@ class OhOccurrence extends OhDateRange implements RefinableCacheableDependencyIn
    */
   public function getMessage(): ?string {
     return $this->message;
+  }
+
+  /**
+   * Set whether this occurrence is open.
+   *
+   * @param bool $open
+   *   Whether this occurrence is open.
+   *
+   * @return $this
+   *   Return object for chaining.
+   */
+  public function setIsOpen(bool $open) {
+    $this->open = $open;
+    return $this;
+  }
+
+  /**
+   * Get whether this occurrence is open.
+   *
+   * @return bool
+   *   Whether this occurrence is open.
+   */
+  public function isOpen(): bool {
+    return $this->open;
   }
 
 }
